8b36c2a70e237748f3226b3c5f6130041e983c9d
Erick Chacon <eral.th07@gmail.com>
2022-06-13 22:45:52 -0500

    regular knots notes

--- src/meshes/regularknots.jl
+++ src/meshes/regularknots.jl
@@ -2,7 +2,8 @@
     RegularKnots(lower, upper, ni, nl, nr)
 
 Defines a RegularKnots struct having `ni` knots between `lower` and `upper`, and with
-additional `nl` knots at the left and `nr` knot at the right.
+additional `nl` knots at the left and `nr` knots at the right. In total, it has `ni + nl +
+nr + 2` knots because it includes `lower` and `upper` as knots.
 """
 struct RegularKnots
     lower::Float64
@@ -12,7 +13,8 @@ struct RegularKnots
     nr::Int
 end
 
-# build knots as a StepRangeLen type
+# Converts a RegularKnots object to a range using `x.lower` as reference value. It allows
+# to create ranges with comparable knots because it uses the same reference value.
 function Base.range(x::RegularKnots)
     n_knots = x.ni + x.nl + x.nr + 2
     step = (x.upper - x.lower) / (x.ni + 1)
@@ -23,7 +25,8 @@ function Base.range(x::RegularKnots)
     return knotrange
 end
 
-# get index i such as knots[i] ≤ x < knots[i+1]. This is mainly used to compute bsplines.
+# Get index i such as knots[i] ≤ x < knots[i+1]. We use it to identify basis splines of
+# order k that are non-zero at x.
 function get_x_index(x::Number, knots::AbstractRange)
 
     # check if x is inside the range
@@ -33,10 +36,10 @@ function get_x_index(x::Number, knots::AbstractRange)
         return length(knots)
     end
 
-    # compute index is x is inside the range
+    # compute index if x is inside the range
     i = 1 + floor(Int, (x - knots[1]) / step(knots))
 
-    # simple fix in case it fails because of precision
+    # simple fix in case condition knots[i] ≤ x < knots[i+1] is not hold
     if x >= knots[i + 1]
         i = i + 1
     end
