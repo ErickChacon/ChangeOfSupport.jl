09332c7738984add0f4501eb5f600adae1cfbfa7
Erick <eral.th07@gmail.com>
2023-09-15 14:08:13 -0500

    fix compatibility with Meshes.jl

--- docs/literate/10-stochastic-process.jl
+++ docs/literate/10-stochastic-process.jl
@@ -21,7 +21,15 @@ gmrf = RGMRF(bsgrid, 2, 0.01, 1)
 tgrid = CartesianGrid((-100.0,), (100.0,), dims = (100,))
 Bs = basis(tgrid, bs)
 δ = rand(gmrf)
-plot(plot(δ), plot(Bs * δ), ylim = (-10, 10))
+
+marks = centroids(bsgrid)[1]
+fig = MK.Figure(resolution = (1000, 500))
+MK.Axis(fig[1,1], title = "a) Discrete process")
+MK.rangebars!(δ, marks .- step(marks) / 2, marks .+ step(marks) / 2,
+            direction = :x, whiskerwidth = 10, linewidth = 2)
+MK.Axis(fig[1,2], title = "b) Continuous process")
+MK.lines!(centroids(tgrid)[1], Bs * δ)
+MK.save("makie.pdf", fig)
 
 #' ## Two-dimenional process
 
@@ -33,5 +41,13 @@ gmrf = RGMRF(bsgrid, 2, 0.1, 1)
 tgrid = CartesianGrid((-100.0, -100.0), (100.0, 100.0), dims = (300,200))
 Bs = basis(tgrid, bs)
 δ = rand(gmrf)
-plot(plot(bsgrid, δ), plot(tgrid, Bs * δ))
+
+#-
+
+fig = MK.Figure(resolution = (1000, 500))
+MK.Axis(fig[1,1], title = "a) Discrete process")
+viz!(bsgrid, color = δ, showfacets = true)
+MK.Axis(fig[1,2], title = "b) Continuous process")
+viz!(tgrid, color = Bs * δ)
+MK.save("makie.pdf", fig)
 
