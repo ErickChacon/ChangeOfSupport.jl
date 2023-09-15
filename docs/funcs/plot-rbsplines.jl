bd8ed58b7bd51b0e76b2f5b09f84206da7b3aebc
Erick <eral.th07@gmail.com>
2023-09-15 03:56:24 -0500

    custom makie function to plot basis functions and realizations

--- docs/funcs/plot-rbsplines.jl
+++ docs/funcs/plot-rbsplines.jl
@@ -1,16 +1,62 @@
+function boundaryknots(b::RegularBsplines)
+    RegularKnots(b.lower, b.upper, b.df - b.order, 0, 0)
+end
 
-function visualize(bs::RegularBsplines)
+function visualize(bs::RegularBsplines; npoints = 500)
+    # evalute bsplines
+    t = range(bs.lower, bs.upper, length = npoints)
+    Bs = basis(t, bs)
+    # plot
+    fig = MK.Figure(resolution = (800, 500))
+    MK.Axis(fig[1,1], xgridvisible = false)
+    MK.vlines!(range(boundaryknots(bs)), color = :gray, linestyle = :dash, linewidth = 1.5);
+    [MK.lines!(t, Bs[:,i], label = "$i") for i = 1:size(Bs, 2)]
+    MK.current_figure()
+end
+
+function visualize(bs::RegularBsplines, t, Bs::AbstractMatrix)
+    fig = MK.Figure(resolution = (800, 500))
+    MK.Axis(fig[1,1], xgridvisible = false)
+    MK.vlines!(range(boundaryknots(bs)), color = :gray, linestyle = :dash, linewidth = 1.5);
+    [MK.lines!(t, Bs[:,i], label = "$i") for i = 1:size(Bs, 2)]
+    MK.current_figure()
+end
+
+function visualize(bs::RegularBsplines, tgrid::CartesianGrid{1}, Bc::AbstractMatrix)
     # evalute bsplines
     t = range(bs.lower, bs.upper, length = 500)
     Bs = basis(t, bs)
-    # plot
-    Mke.lines(t, Bs[:, 1])
-    for i = 2:size(Bs, 2)
-        Mke.lines!(t, Bs[:, i])
+    # plot aggregated support
+    marks = centroids(tgrid)[1]
+    fig = MK.Figure(resolution = (800, 500))
+    MK.Axis(fig[1,1], xgridvisible = false)
+    for i = 1:size(Bs, 2)
+        MK.lines!(t, Bs[:, i])
+        MK.rangebars!(Bc[:,i], marks .- step(marks) / 2, marks .+ step(marks) / 2,
+            direction = :x, whiskerwidth = 10, linewidth = 2)
     end
-    Mke.current_figure()
+    MK.vlines!(range(tgrid)[1], color = :gray, linestyle = :dash, linewidth = 0.8);
+    MK.current_figure()
 end
 
-visualize(bs)
-Mke.save("plop.pdf", Mke.current_figure())
+function visualize(bs::RegularBsplines, igrid::ChangeOfSupport.RectilinearGrid{1}, Bc::AbstractMatrix)
+    # evalute bsplines
+    t = range(bs.lower, bs.upper, length = 500)
+    Bs = basis(t, bs)
+    # plot aggregated support
+    gridknots = knotset(igrid)[1]
+    # marks = centroids(igrid)[1]
+    fig = MK.Figure(resolution = (800, 500))
+    MK.Axis(fig[1,1], xgridvisible = false)
+    for i = 1:size(Bs, 2)
+        MK.lines!(t, Bs[:, i])
+        MK.rangebars!(Bc[:,i], gridknots[1:end-1], gridknots[2:end],
+            direction = :x, whiskerwidth = 10, linewidth = 2)
+    end
+    MK.vlines!(gridknots, color = :gray, linestyle = :dash, linewidth = 1.2);
+    MK.current_figure()
+end
+
+# visualize(bs)
+# MK.save("plop.pdf", MK.current_figure())
 
--- docs/literate/04-rbsplines.jl
+++ docs/literate/04-rbsplines.jl
@@ -6,14 +6,22 @@ using Plots
 import CairoMakie as MK
 using AlgebraOfGraphics
 using ChangeOfSupport
-gr(dpi = 250)
+
+include("funcs/plot-rbsplines.jl")
+
+# gr(dpi = 250)
+
 
 # ### Define a regular B-splines
 #
 # Regular B-splines can be defined by providing the boundary knots (minimum and maximum),
 # the degree of freedom and de order.
+
 n = 7
 bs = RegularBsplines(-10, 10, n, 3)
+#-
+visualize(bs, npoints = 500)
+MK.save("makie.pdf", MK.current_figure()) #src
 
 # ### Evaluating the Bsplines (i)
 #
@@ -24,77 +32,22 @@ basis(10.0, bs)
 
 # ### Evaluating the Bsplines (ii)
 # Usually we want to evaluate the basis functions at a set of values.
+
 t = range(-10, 10, length = 500)
 Bs = basis(t, bs)
+#-
+visualize(bs, t, Bs)
+MK.save("makie.pdf", MK.current_figure()) #src
 
-# Bss = sparse(Bs)
-# length(findall(!iszero, Bss)) / prod(size(Bss))
-
-function boundaryknots(b::RegularBsplines)
-    RegularKnots(b.lower, b.upper, b.df - b.order, 0, 0)
-end
-
-ENV["GKSwstype"] = "100"
-# GKS: can't connect to GKS socket application
-# qt.qpa.xcb: XKeyboard extension not present on the X server
-# There is no XRandR 1.2 and later version available. There will be only fake screen(s) to use.
-# connect: Connection refused
-# GKS: can't connect to GKS socket application
-
-# p1 = plot(t, Bs, lw = 1, legend = false, title = "(a) B-splines");
-# plot!(p1, range(boundaryknots(bs)), st = :vline, color = :gray, ls = :dash, lw = 0.5);
-# savefig(p1, "plot.pdf")
-
-# # fig = MK.series(t, Bs', color = :Set1, axis = (title =  "(a) B-splines",));
-# fig = MK.series(t, Bs', color = :Set1,
-#     axis = (title =  "(a) B-splines", xgridvisible = false));
-# MK.vlines!(range(boundaryknots(bs)), color = :gray, linestyle = :dash, linewidth = 1.2);
-# MK.save("makie.pdf", fig) #src
-
-fig = MK.Figure(resolution = (800, 500))
-MK.Axis(fig[1,1], title =  "(a) B-splines", xgridvisible = false)
-MK.vlines!(range(boundaryknots(bs)), color = :gray, linestyle = :dash, linewidth = 1.5);
-[MK.lines!(t, Bs[:,i], label = "$i") for i = 1:size(Bs, 2)]
-MK.save("makie.pdf", fig) #src
-
-# MK.vlines(range(boundaryknots(bs)), color = :gray, linestyle = :dash, linewidth = 1.5,
-#     figure = (; resolution = (800, 500)),
-#     axis = (; title = "(a) B-splines", xgridvisible = false)
-# )
-# [MK.lines!(t, Bs[:,i], label = "$i") for i = 1:size(Bs, 2)]
-# MK.save("makie.pdf", fig) #src
-
-
-# MK.Legend(fig[1,2], ax, "Basis")
 # ### Evaluating the Bsplines integral from -∞ to x
+
 Bi = integral(t, bs)
-
-# p2 = plot(t, Bi, lw = 1, legend = false, title = "(b) B-splines Integral");
-# plot!(p2, range(boundaryknots(bs)), st = :vline, color = :gray, ls = :dash, lw = 0.5)
-
-# fig = MK.series(t, Bi', color = :Set1, axis = (title =  "(b) B-splines Integral",));
-# MK.vlines!(range(boundaryknots(bs)), color = :gray, linestyle = :dash, linewidth = 0.8);
-# MK.save("makie.pdf", fig) #src
-
-fig = MK.Figure(resolution = (800, 500))
-MK.Axis(fig[1,1], title =  "(b) B-splines Integral", xgridvisible = false)
-MK.vlines!(range(boundaryknots(bs)), color = :gray, linestyle = :dash, linewidth = 1.5);
-[MK.lines!(t, Bi[:,i], label = "$i") for i = 1:size(Bs, 2)]
-MK.save("makie.pdf", fig) #src
+#-
+visualize(bs, t, Bi)
+MK.save("makie.pdf", MK.current_figure()) #src
 
 # When showing the basis functions and the integrals with can see that the integrals reach
 # their maximum after the last knot where the basis is not zero.
-# p = plot(p1, p2, layout = (2, 1), size = (600, 800));
-# savefig(p, "plot.pdf")
-
-# fig = MK.Figure(resolution = (800, 900))
-# ax1 = MK.Axis(fig[1,1], title =  "(a) B-splines")
-# MK.series!(ax1, t, Bs', color = :Set1);
-# MK.vlines!(range(boundaryknots(bs)), color = :gray, linestyle = :dash, linewidth = 0.8);
-# ax2 = MK.Axis(fig[2,1], title =  "(b) B-splines Integral")
-# MK.series!(ax2, t, Bi', color = :Set1);
-# MK.vlines!(range(boundaryknots(bs)), color = :gray, linestyle = :dash, linewidth = 0.8);
-# MK.save("makie.pdf", fig) #src
 
 fig = MK.Figure(resolution = (800, 900))
 MK.Axis(fig[1,1], title =  "(a) B-splines")
@@ -105,109 +58,32 @@ MK.vlines!(range(boundaryknots(bs)), color = :gray, linestyle = :dash, linewidth
 [MK.lines!(t, Bi[:,i], label = "$i") for i = 1:size(Bi, 2)]
 MK.save("makie.pdf", fig) #src
 
-
 # ### Measure the Bsplines over a regular grid (low resolution)
+
 tgrid = CartesianGrid(Point(-10.0), Point(10.0), dims = (10,))
 Bc = basis(tgrid, bs)
-
-# marks = centroids(tgrid)[1]
-# cols = transpose(1:size(Bc)[2])
-# p = plot(t, Bs, lw = 1, legend = false, grid = false);
-# scatter!(marks, Bc, xerr = step(marks) / 2, legend = false,
-#         msc = cols, color = cols, ms = 4);
-# vline!(range(tgrid)[1], c = :gray, lw = 0.5, ls = :dash)
-# savefig(p, "plot.pdf")
-
-# fig = MK.series(t, Bs')
-fig = MK.Figure(resolution = (800, 500))
-MK.Axis(fig[1, 1])
-for i = 1:size(Bs, 2)
-    MK.lines!(t, Bs[:, i])
-    MK.rangebars!(Bc[:,i], marks .- step(marks) / 2, marks .+ step(marks) / 2,
-        direction = :x, whiskerwidth = 10, linewidth = 2)
-end
-MK.vlines!(range(tgrid)[1], color = :gray, linestyle = :dash, linewidth = 0.8);
-MK.save("makie.pdf", fig) #src
-
-# using Tables
-# plt = data(Tables.table(Bs)) * mapping([:Column1, :Column2, :Columns])
-#
-# fig = MK.series(t, Bs')
-# # MK.scatter!(marks, Bc[:,1])
-# # MK.vlines!(range(tgrid)[1])
-# MK.save("makie.pdf", fig) #src
-#
-
-
+# -
+visualize(bs, tgrid, Bc)
+MK.save("makie.pdf", MK.current_figure()) #src
 
 # ### Measure the Bsplines over a regular grid (high resolution)
 tgrid = CartesianGrid(Point(-10.0), Point(10.0), dims = (40,))
 Bc = basis(tgrid, bs)
-
-# marks = centroids(tgrid)[1]
-# cols = transpose(1:size(Bc)[2])
-# p = plot(t, Bs, lw = 1, legend = false, grid = false);
-# scatter!(marks, Bc, xerr = step(marks) / 2, legend = false,
-#         msc = cols, color = cols, ms = 2);
-# vline!(range(tgrid)[1], c = :gray, lw = 0.5, ls = :dash)
-# savefig(p, "plot.pdf")
-
-fig = MK.Figure(resolution = (800, 500))
-MK.Axis(fig[1, 1])
-for i = 1:size(Bs, 2)
-    MK.lines!(t, Bs[:, i])
-    MK.rangebars!(Bc[:,i], marks .- step(marks) / 2, marks .+ step(marks) / 2,
-        direction = :x, whiskerwidth = 10, linewidth = 2)
-end
-MK.vlines!(range(tgrid)[1], color = :gray, linestyle = :dash, linewidth = 0.8);
-MK.save("makie.pdf", fig) #src
-
+# -
+visualize(bs, tgrid, Bc)
+MK.save("makie.pdf", MK.current_figure()) #src
 
 # ### Measure the Bsplines over a irregular grid (low resolution)
 igrid = ChangeOfSupport.RectilinearGrid((-10.0,), (10.0,), (-10 .+ rand(10) * 20,))
 Bc = basis(igrid, bs)
-
-gridknots = knotset(igrid)[1]
-marks = centroids(igrid)[1]
-cols = transpose(1:size(Bc)[2])
-p = plot(t, Bs, lw = 1, legend = false, grid = false);
-scatter!(marks, Bc, xerr = diff(gridknots) / 2, legend = false,
-        msc = cols, color = cols, ms = 4);
-vline!(gridknots, c = :gray, lw = 0.5, ls = :dash)
-savefig(p, "plot.pdf")
-
-fig = MK.Figure(resolution = (800, 500))
-MK.Axis(fig[1, 1], xgridvisible = false)
-for i = 1:size(Bs, 2)
-    MK.lines!(t, Bs[:, i])
-    MK.rangebars!(Bc[:,i], gridknots[1:end-1], gridknots[2:end],
-        direction = :x, whiskerwidth = 10, linewidth = 2)
-end
-MK.vlines!(gridknots, color = :gray, linestyle = :dash, linewidth = 1.2);
-MK.save("makie.pdf", fig) #src
-
+# -
+visualize(bs, igrid, Bc)
+MK.save("makie.pdf", MK.current_figure()) #src
 
 # ### Measure the Bsplines over a irregular grid (high resolution)
 igrid = ChangeOfSupport.RectilinearGrid((-10.0,), (10.0,), (-10 .+ rand(40) * 20,))
 Bc = basis(igrid, bs)
-
-gridknots = knotset(igrid)[1]
-marks = centroids(igrid)[1]
-cols = transpose(1:size(Bc)[2])
-p = plot(t, Bs, lw = 1, legend = false, grid = false);
-scatter!(marks, Bc, xerr = diff(gridknots) / 2, legend = false,
-        msc = cols, color = cols, ms = 2);
-vline!(gridknots, c = :gray, lw = 0.5, ls = :dash)
-savefig(p, "plot.pdf")
-
-fig = MK.Figure(resolution = (800, 500))
-MK.Axis(fig[1, 1], xgridvisible = false)
-for i = 1:size(Bs, 2)
-    MK.lines!(t, Bs[:, i])
-    MK.rangebars!(Bc[:,i], gridknots[1:end-1], gridknots[2:end],
-        direction = :x, whiskerwidth = 10, linewidth = 2)
-end
-MK.vlines!(gridknots, color = :gray, linestyle = :dash, linewidth = 1.2);
-MK.save("makie.pdf", fig) #src
-
+# -
+visualize(bs, igrid, Bc)
+MK.save("makie.pdf", MK.current_figure()) #src
 
