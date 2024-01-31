1513b17b0d8c1b937f0d21e3da7e4074dfa50c01
Erick <eral.th07@gmail.com>
2024-01-31 11:45:33 +0300

    thinning

--- src/mcmc.jl
+++ src/mcmc.jl
@@ -1,7 +1,7 @@
 
 # Mcmc sampling
 
-for filename in ["mcmc.jl", "mcmc-model.jl"]
+for filename in ["mcmc.jl", "mcmc-model.jl", "mcmc-model-y.jl"]
     include(joinpath("mcmc", filename))
 end
 
--- src/mcmc/mcmc-model-y.jl
+++ src/mcmc/mcmc-model-y.jl
@@ -1,4 +1,4 @@
-function sample_model_y(y, Bw, Pw, κw, id; binit = nothing, σ²yinit = nothing, δwinit = nothing, niter = 10)
+function sample_model_y(y, Bw, Pw, κw, id; binit = nothing, σ²yinit = nothing, δwinit = nothing, nsamples = 10, thin = 1, thinz = 1)
 
     # dimensions
     K = length(y)
@@ -29,11 +29,13 @@ function sample_model_y(y, Bw, Pw, κw, id; binit = nothing, σ²yinit = nothing
     zlower = [ifelse.(y[k] .== 0, nothing, 0) for k in 1:K]
     zupper = [ifelse.(y[k] .== 1, nothing, 0) for k in 1:K]
 
+    niter = (nsamples - 1) * thin + 1
+    nsamplesz = length(1:thinz:niter)
     # initialize samples
-    δw_samples = zeros(qw, niter)
-    z_samples = [zeros(n[k], niter) for k in 1:K]
-    b_samples = zeros(K, niter)
-    σ²y_samples = zeros(K, niter)
+    δw_samples = zeros(qw, nsamples)
+    z_samples = [zeros(n[k], nsamplesz) for k in 1:K]
+    b_samples = zeros(K, nsamples)
+    σ²y_samples = zeros(K, nsamples)
 
     δw_samples[:, 1] = δw
     [z_samples[k][:, 1] = z[k] for k in 1:K]
@@ -42,14 +44,22 @@ function sample_model_y(y, Bw, Pw, κw, id; binit = nothing, σ²yinit = nothing
 
     # mcmc
     for i = 2:niter
+        saveiterz = (i-1) % thinz == 0
+        saveidz = div(i-1, thinz) + 1
+
+        saveiter = (i-1) % thin == 0
+        saveid = div(i-1, thin) + 1
+
         # sample z
         [resid[k] -= z[k] for k in 1:K]
         μz = [-resid[k] for k in 1:K]
         varz = σ²y
         Z = [truncated.(Normal.(μz[k], sqrt.(varz[k])), zlower[k], zupper[k]) for k in 1:K]
         z  = [rand.(Z[k]) for k in 1:K]
-        for k = 1:K
-            z_samples[k][:, i] = z[k]
+        if saveiterz
+            for k = 1:K
+                z_samples[k][:, saveidz] = z[k]
+            end
         end
         [resid[k] += z[k] for k in 1:K]
 
@@ -60,7 +70,9 @@ function sample_model_y(y, Bw, Pw, κw, id; binit = nothing, σ²yinit = nothing
         aux = sum([σ²y[k]^(-1) * Bw[k]' * resid[k] for k in 1:K])
         μw =  CQw.UP \ (CQw.PtL \ aux)
         δw = μw + CQw.UP \ randn(qw)
-        δw_samples[:, i] = δw
+        if saveiter
+            δw_samples[:, saveid] = δw
+        end
         [resid[k] -= Bw[k] * δw for k in 1:K]
 
         # sample β
@@ -68,14 +80,18 @@ function sample_model_y(y, Bw, Pw, κw, id; binit = nothing, σ²yinit = nothing
         CQᵦ = cholesky(Qᵦ)
         μᵦ = CQᵦ.U \ (CQᵦ.L \ sum([σ²y[k]^(-1) * A[k]' * (z[k] - Bw[k] * δw) for k in 1:K]))
         b = μᵦ + CQᵦ.U \ randn(K)
-        b_samples[:, i] = b
+        if saveiter
+            b_samples[:, saveid] = b
+        end
         resid = [z[k] - A[k] * b - Bw[k] * δw for k in 1:K]
 
         # sample σ²y
         a_σ²y = sigma2_a_prior .+ n / 2
         b_σ²y = [sigma2_b_prior + sum(resid[k].^2) / 2 for k in 1:K]
         σ²y = rand.(InverseGamma.(a_σ²y, b_σ²y))
-        σ²y_samples[:, i] = σ²y
+        if saveiter
+            σ²y_samples[:, saveid] = σ²y
+        end
     end
 
     δw_samples, b_samples, σ²y_samples, z_samples
