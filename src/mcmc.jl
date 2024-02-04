0ac796773e0d449a79fd2acc24dc8e26bb19ec84
Erick <eral.th07@gmail.com>
2024-02-04 18:44:35 +0300

    standardise model

--- src/mcmc.jl
+++ src/mcmc.jl
@@ -1,7 +1,7 @@
 
 # Mcmc sampling
 
-for filename in ["mcmc-aux.jl", "mcmc.jl", "mcmc-model.jl", "mcmc-model-y.jl"]
+for filename in ["mcmc-aux.jl", "mcmc.jl", "mcmc-model.jl", "mcmc-model-y.jl", "mcmc-model-standard.jl"]
     include(joinpath("mcmc", filename))
 end
 
